ae54e03cebc4273eef4ab22cad24e9c10ae1a18f
Will Keleher <will.keleher@gmail.com>
2013-09-18 11:41:08 -0600

    getElementsByClassName is working and it's pretty

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,41 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var count = 0;
-var results = [];
 
-//TODO make sure this just goes through once.
-
-var getElementsByClassName = function(className, node) {
+var getElementsByClassName = function(className, current) {
   
-  var current = node || document.body;
-  
-  if (current === document.body){
-    for (var i = 2; i < document.body.childNodes.length; i++){
-      getElementsByClassName(className, document.body.childNodes[i]);
-    }
-  }
-  else {
+  current = current || document.body;
+  var results = [];
+  var search = function(className, current){
     if (current.childNodes){
       for (var i = 0; i < current.childNodes.length; i++){
-        getElementsByClassName(className, current.childNodes[i]);
+        search(className, current.childNodes[i]);
       } 
     }
-    results.push(current);
-  }
-  
-  if (current == document.body){
-  var answer = [];
-    for (var i = 0; i < results.length; i++){
-      if (results[i].classList){
-        for (var j = 0; j < results[i].classList.length; j++) {
-          if (results[i].classList[j] == className){
-            answer.push(results[i]);
-          }
+    if (current.classList) {
+      for (var j = 0; j < current.classList.length; j++) {
+        if (current.classList[j] == className){
+          results.push(current);
         }
       }
-    }
-    results = [];
-    return answer;  
-  }
+    } 
+  };
+  search(className, current);
+  return results; 
 };
